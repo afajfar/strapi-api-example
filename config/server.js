c3f11c395d14e6793420f3ca2613523ddfe934f8
anzefajfar <anze.fajfar@agiledrop.com>
2020-10-01 10:09:24 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '47cef7443a076687e27ef6530b14a5ee'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '076f0bdb-9b08-42f7-9e74-b6841a74c803'
+};
